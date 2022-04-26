da1fd39769bd76b0230bb7bd4f798f71f491ec92
Gladis Ariane <gladisar@student.21-school.ru>
2022-04-27 00:28:55 +0300

    Testing hard

--- src/test.c
+++ src/test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include "s21_string.h"
 
-int main() {
-    printf("");
-    return 0;
+int main(void) {
+    size_t p;
+	char *tested = "this is test";
+	char *a = "shit";
+	p = strspn(tested,a);
+    printf("%zu\n", p);
+
+
+	p = s21_strspn(tested, a);
+	printf("%zu", p);
+
+	return 0;
 }
