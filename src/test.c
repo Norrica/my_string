9ee06d1a1dfef4ce9798e379beae06f365012b55
Jarrus Abram <jarrusab@ev-m3.kzn.21-school.ru>
2022-04-27 19:01:25 +0300

    Added tests for s21_strspn

--- src/test.c
+++ src/test.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "s21_string.h"
+#include "s21_string.H"
+#include <check.h>
+
+START_TEST(test_strspn) {
+    char *test_str1 = "Escape Meta Alt Control Shift";
+    char *test_str2 = "Escape from school 21";
+    ck_assert_int_eq(strspn(test_str1, test_str2), s21_strspn(test_str1, test_str2));
+//    ck_assert(1 == 2);
+//    ck_assert_msg(1 == 2, "Was expecting 2, but got 1");
+}
+END_TEST
+
+Suite* string_suite(void) {
+    Suite* s;
+    TCase *tc_core;
+    s = suite_create("s21_string");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_strspn);
+    suite_add_tcase(s, tc_core);
+    return s;
+}
 
 int main(void) {
     size_t p;
-	char *tested = "this is test";
-	char *a = "shit";
-	p = strspn(tested,a);
-    printf("%zu\n", p);
-
-
-	p = s21_strspn(tested, a);
-	printf("%zu", p);
-
-	return 0;
+    int number_failed = 0;
+    Suite* s;
+    SRunner *sr;
+    s = string_suite();
+    sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+//	return 0;
 }
