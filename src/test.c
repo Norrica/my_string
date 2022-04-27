500471324757b88d5bb50108b87cf522d7ad201d
Gladis Ariane <gladisar@student.21-school.ru>
2022-04-27 18:52:58 +0300

    strspn seems to work

--- src/test.c
+++ src/test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include "s21_string.h"
 
-int main() {
-    printf("");
-    return 0;
+int main(void) {
+	s21_size_t p;
+	char *tested = "this is a test";
+	char *a = "shit ";
+	p = strspn(tested,a);
+	printf("%zu\n", p);
+
+
+	p = s21_strspn(tested, a);
+	printf("%zu", p);
+
+	return 0;
 }
