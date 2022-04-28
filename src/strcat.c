e0fd4958cead60a3c6a0e729ebf44c7aea06ea91
Ululate Leoma <ululatel@ev-l4.kzn.21-school.ru>
2022-04-28 16:45:53 +0300

    strcat working and tested

--- src/strcat.c
+++ src/strcat.c
@@ -1,10 +1,16 @@
 #include "s21_string.h"
 
 char *s21_strcat(char *dest, const char *src) {
-    char *ptr = dest + s21_strlen(dest);
-    while (*src !='\0') {
-        *ptr++ = *src++;
+    if ((dest == NULL) && (src == NULL))
+        return NULL;
+
+    char *start = dest;
+    while (*start != '\0') {
+        start++;
     }
-    *ptr = '\0';
+    while (*src != '\0') {
+        *start++ = *src++;
+    }
+    *start = '\0';
     return dest;
 }
