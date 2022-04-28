67e96ee60a3033b73049f8e9622f5ecb6fd7ea2b
Ululate Leoma <ululatel@ev-l4.kzn.21-school.ru>
2022-04-28 15:32:07 +0300

    almost done - need testing

--- src/strcat.c
+++ src/strcat.c
@@ -2,20 +2,34 @@
 The strcat() function concatenates the destination string and the source string, 
 and the result is stored in the destination string.
 --------------------------------------------
-char str1[100] = "This is ", str2[] = "programiz.com";
-strcat(str1, str2);
-puts(str1);
+    char str1[100] = "This is ", str2[] = "programiz.com";
+    strcat(str1, str2);
+    puts(str1);
+--------------------------------------------
 >> This is programiz.com
 --------------------------------------------
 */
 
 #include "s21_string.h"
+s21_size_t s21_strlen(const char *str)
+{
+    s21_size_t len = 0;
+    for(; str[len]; len++);
+    return len + 1;
+}
 
 char *s21_strcat(char *dest, const char *src) {
     char *ptr = dest + s21_strlen(dest);
     while (*src !='\0') {
-        *ptr++ = *src++;
-    }
+        *ptr++ = *src++; }
     *ptr = '\0';
     return dest;
+}
+
+int main() {
+    char str1[100] = "This is ", str2[] = "programiz.com";
+    printf("%ld", s21_strlen(str2));
+    s21_strcat(str1, str2);
+    puts(str1);
+    return 0;
 }
--- src/strcmp.c
+++ src/strcmp.c
@@ -1,3 +1,40 @@
+/*
+The strcmp() compares two strings character by character. 
+If the strings are equal, the function returns 0.
+--------------------------------------------
+    char c1[] = "abcd";
+    char c2[] = "abcd";
+    char c3[] = "abcde";
+    printf("%d", s21_strcmp(c1, c2));
+    printf("\n");
+    printf("%d", s21_strcmp(c1, c3));
+    printf("\n");
+--------------------------------------------
+>>>0
+>>>-101
+*/
 #include "s21_string.h"
+#include <string.h>
 
-int s21_strcmp(const char *str1, const char *str2)
+
+
+int s21_strcmp(const char *str1, const char *str2) {
+    while(*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++; }
+    return *(const unsigned char*)str1 - *(const unsigned char*)str2;
+}
+
+int main() {
+    char c1[] = "abcd";
+    char c2[] = "abcd";
+    char c3[] = "abcde";
+    printf("%d", s21_strcmp(c1, c2));
+    printf("\n");
+    printf("%d", s21_strcmp(c1, c3));
+    printf("\n");
+    printf("%d", strcmp(c1, c2));
+    printf("\n");
+    printf("%d", strcmp(c1, c3));    
+    return 0;
+}
