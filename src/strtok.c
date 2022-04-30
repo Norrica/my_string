79ca93b79ef6c6dcced2a9e519cd4e9e1c873bcd
ululatel <ululatel@student.21-school.ru>
2022-04-30 13:29:49 +0300

    tryied strtok

--- src/strtok.c
+++ src/strtok.c
@@ -1,6 +1,17 @@
 #include "s21_string.h"
+#include <string.h>
 
 char *s21_strtok(char *str, const char *delim) {
-    char *result = malloc(sizeof)
+    s21_size_t len = s21_strlen(str);
+    //char *result = malloc(len*sizeof(char));
+    for (int i = 0; str[i] != '\0'; i++) {
 
+
+}
+
+int main() {
+    char a[100] = "cccab=cccefgc";
+    char *c;
+    *c = 'c-=/';
+    s21_strtok(a, c);
 }
