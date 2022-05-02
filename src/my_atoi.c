63fa466d0dbc70c7e033ff72f73aff3b1f7142f8
ululatel <ululatel@student.21-school.ru>
2022-05-02 14:19:40 +0300

    added test case for atoi func inside of it

--- src/my_atoi.c
+++ src/my_atoi.c
@@ -14,3 +14,18 @@ int my_atoi (const char *c) {
         res = res * 10 + *ch++ - '0'; }
     return res*sign;
 }
+
+// TODO
+// !Проверить!
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include "s21_string.h"
+//
+//int main() {
+//    int counter = 0, len1 = 0;
+//    char c[100] = "-12454545454545445757564521455852646565656565656565656565656565";
+//    printf("my: %d\n", my_atoi(c));
+//    printf("original: %d", atoi(c));
+//    return 0;
+//}
